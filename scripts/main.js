905994c6da5f5e48a32772aacf2d0971e2b5f6fd
ActuallyAJL <zero3260@hotmail.com>
2022-03-29 11:57:22 -0500

    converted to a JSON API database

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,33 @@
-import { getJournalEntries } from './journalData.js';
-import { JournalEntryComponent } from './helper.js';
+import { getPosts } from "./journalData.js";
+import { JournalEntryComponent } from "./helper.js";
 
-const entrySpace = document.querySelector('.journal-previous-entries');
-const header = document.querySelector('#journal-greeting');
+const entrySpace = document.querySelector(".journal-previous-entries");
+const header = document.querySelector("#journal-greeting");
 
 const renderEntries = () => {
-    let entryArray=getJournalEntries();
-    for (let entry of entryArray) {
-        entrySpace.innerHTML += `
+  getPosts().then((entryArray) => {
+      const something = entryArray
+      .map((singleEntry) => {
+        return `
             <article class = 'singleEntry'>
-                <h3 class="journal-entry-date">${entry.date}</h3>
-                <h3 class="journal-entry-concepts">${entry.concept}</h3>
+                <h3 class="journal-entry-date">${singleEntry.date}</h3>
+                <h3 class="journal-entry-concepts">${singleEntry.concept}</h3>
                 <article class="journal-entry-body">
-                    <p>${entry.entry}</p>
-                    <p>I'm feeling ${entry.mood} about it</p>
+                    <p>${singleEntry.entry}</p>
+                    <p>I'm feeling ${singleEntry.mood} about it</p>
                 </article>
                 <button>EDIT</button><button>DELETE</button>
             </article>
-        `;
-    }
-}
+            `;
+      }).join(' ');
+      entrySpace.innerHTML = something;
+  });
+};
 
 const renderGreeting = () => {
-    const greetingHTML = `Hello, Alex!, <br />Welcome to your private journal<br />Today's date is ${JournalEntryComponent()}<br />Tell me about your day:`;
-    return greetingHTML;
-}
+  const greetingHTML = `Hello, Alex!, <br />Welcome to your private journal<br />Today's date is ${JournalEntryComponent()}<br />Tell me about your day:`;
+  return greetingHTML;
+};
 
 header.innerHTML = renderGreeting();
 renderEntries();
